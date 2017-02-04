82f792f14c6404ab9bac57928c288c0f98214026
Jayon Huh <huhjayon@gmail.com>
2017-02-03 19:25:24 -0800

    fixed bug where test was failing due to missing forms in test

--- test/org/tasks/FormsSpec.scala
+++ test/org/tasks/FormsSpec.scala
@@ -13,7 +13,7 @@ class FormsSpec extends JUnitSuite with Matchers {
 
   @Test
   def taskForm(): Unit = {
-    val data: Map[String, String] = Map("title" -> "some title", "note" -> "feed the cats")
+    val data: Map[String, String] = Map("title" -> "some title", "note" -> "feed the cats", "isDone" -> "false", "dueDate" ->  "2017-07-10")
     val task: Task = Forms.taskForm.bind(data).get
     task.title should be ("some title")
     task.note should be ("feed the cats")
